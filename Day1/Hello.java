b9f75db163657195feb8770da5c709d10c384453
saiprakashreddy916 <47684511+saiprakashreddy916@users.noreply.github.com>
2022-12-22 16:53:31 -0600

    First Commit

---
+++ Day1/Hello.java
@@ -0,0 +1,23 @@
+package Day1;
+class Hello{
+  public static void main(String[] args)
+  {
+     System.out.println("Name: SaiPrakash Reddy"+'\n'+"Mobile: 9376728405"+'\n'+"Address: 545 Ranch Trail, Unit 184"+'\n'+"city: Irving"+'\n'+"state: Texas"+'\n'+"country: USA"+'\n'+"zip: 75063");
+     int a = 10;
+     System.out.println(a);
+     byte b = 12;
+     System.out.println(b);
+     short c = 15;
+     System.out.println(c);
+     long d = 56186435;
+     System.out.println(d);
+     float e = 51.555f;
+     System.out.println(e);
+     double f = 5652.256;
+     System.out.println(f);
+     boolean g = true;
+     System.out.println(g);
+     char h = 'J';
+     System.out.println(h);
+  }
+}
